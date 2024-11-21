893e56c29643adba0d0ddfbf4fb8665835423365
escaay <qwj2790751195@gmail.com>
2024-11-21 23:08:11 +0800

    feat: 文章点赞和文章评论点赞的删除和创建

---
+++ app/api/article/deleteArticleComment/route.ts
@@ -0,0 +1,28 @@
+import { type NextRequest } from 'next/server'
+import { PrismaClient } from '@prisma/client'
+export async function POST(req: NextRequest) {
+    const body = await req.json()
+    const {articlecCommentId} = body
+    const prisma = new PrismaClient()
+    // 线上的address是["线上", "全部"]， isDetail是否详情（评论信息等）
+    try {
+    const row = await prisma.article_comment.delete({
+        where: {
+          articlecCommentId
+        }
+    })
+    return Response.json({
+        code: 200,
+        message: '成功',
+        data: row
+    })
+} catch (e) {
+  console.log(e)
+  return Response.json({
+      code: 500,
+      messsage: "失败",
+    });
+  } finally {
+    prisma.$disconnect();
+  }
+}
---
+++ app/api/article/deleteArticleLike/route.ts
@@ -0,0 +1,27 @@
+import { type NextRequest } from 'next/server'
+import { PrismaClient } from '@prisma/client'
+export async function POST(req: NextRequest) {
+    const body = await req.json()
+    const {articleLikeId} = body
+    const prisma = new PrismaClient()
+    try {
+    const row = await prisma.article_like.delete({
+        where: {
+          articleLikeId
+        }
+    })
+    return Response.json({
+        code: 200,
+        message: '成功',
+        data: row
+    })
+} catch (e) {
+  console.log(e)
+  return Response.json({
+      code: 500,
+      messsage: "失败",
+    });
+  } finally {
+    prisma.$disconnect();
+  }
+}
