6dff4c76f9e6126e15f6e55cc1f697613cc7bfc5
escaay <qwj2790751195@gmail.com>
2024-12-06 17:29:09 +0800

    fix: 修复用户点赞列表时间顺序错误

--- app/api/article/queryLikeList/route.ts
+++ app/api/article/queryLikeList/route.ts
@@ -3,10 +3,18 @@ import { PrismaClient } from "@prisma/client";
 export async function POST(req: NextRequest) {
   const body = await req.json();
   const prisma = new PrismaClient();
-  const { userId } = body;
+  const { userId, skip } = body;
+  console.log(skip)
+  const PAGE_SIZE = 5;
+  // 因为是双表查询，不能用常规的skip
+  // 判断是否10, 20，而不是5, 15等数字
+  const isDobule = String(skip).slice(-1) === "0";
   try {
     const articleLikeList = await prisma.article_like.findMany({
       where: { articleSenderId: userId },
+      skip: !isDobule ? ((skip - 5) < 0 ? 0 : (skip - 5)) : skip,
+      // 要拿2倍的数据出来，才能保证时间顺序，后续再优化
+      take: PAGE_SIZE * 2,
       select: {
         articleLikeId: true,
         articleSenderId: true,
@@ -20,16 +28,16 @@ export async function POST(req: NextRequest) {
             avatarURL: true,
             currentAddress: true,
             gender: true,
-            age: true
-          }
+            age: true,
+          },
         },
         article: {
           select: {
             articleId: true,
             textContent: true,
             imageUrlList: true,
-          }
-        }
+          },
+        },
       },
       orderBy: {
         // 按照创建时间降序排序（发布评论的时间）
@@ -37,6 +45,8 @@ export async function POST(req: NextRequest) {
       },
     });
     const articleCommentLikeList = await prisma.article_comment_like.findMany({
+      take: PAGE_SIZE * 2,
+      skip: !isDobule ? ((skip - 5) < 0 ? 0 : (skip - 5)) : skip,
       where: {
         commentSenderId: userId,
       },
@@ -53,27 +63,43 @@ export async function POST(req: NextRequest) {
             avatarURL: true,
             currentAddress: true,
             gender: true,
-            age: true
-          }
+            age: true,
+          },
         },
         article: {
           select: {
-            articleId: true
-          }
+            articleId: true,
+          },
         },
         articleComment: {
           select: {
             articleCommentId: true,
             textContent: true,
             imageURL: true,
-          }
-        }
+          },
+        },
       },
       orderBy: {
         // 按照创建时间排序（发布评论的时间）
         createTime: "desc",
       },
     });
+
+    const articleLikeListCount = await prisma.article_like.count({
+      where: { articleSenderId: userId },
+    });
+
+    const articleCommentLikeListCount = await prisma.article_comment_like.count(
+      {
+        where: {
+          commentSenderId: userId,
+        },
+      }
+    );
+
+    const likeList = [...articleLikeList, ...articleCommentLikeList];
+    // 降序
+    likeList.sort((a, b) => b.createTime.getTime() - a.createTime.getTime());
 
     // 把个人的所有未读的点赞信息设为已读（动态点赞，评论点赞）
     await prisma.article_like.updateMany({
@@ -92,16 +118,22 @@ export async function POST(req: NextRequest) {
       },
     });
 
+    const result = isDobule ? likeList.slice(0, 5) : likeList.slice(5, 10);
+    // console.log(likeList.map(item => {
+    //   const result = {...item}
+    //   delete result.sender
+    //   return result
+    // }))
     return Response.json({
       code: 200,
       message: "成功",
-      data: { articleLikeList, articleCommentLikeList },
+      data: {likeList: result, likeListCount: articleLikeListCount + articleCommentLikeListCount},
     });
   } catch (e) {
     console.log(e);
     return Response.json({
       code: 500,
-      messsage: "失败",
+      message: "失败",
     });
   } finally {
     prisma.$disconnect();
